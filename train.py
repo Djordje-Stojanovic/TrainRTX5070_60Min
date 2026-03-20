77cc6c15f564f7af64185e0e1782eb78be5bcd31
Djordje Stojanovic <djordjestojanovic1992@gmail.com>
2026-03-20 13:11:28 +0100

    revert: undo label_smoothing

--- train.py
+++ train.py
@@ -593,7 +593,6 @@ class GPT(nn.Module):
                 targets.view(-1),
                 ignore_index=-1,
                 reduction=reduction,
-                label_smoothing=0.1,
             )
             return loss
         return logits
